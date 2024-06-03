84e900f941c58326872e245e174fa074a39a3c79
Railcka <zena1337@mail.ru>
2024-06-04 01:03:17 +0300

    account and recipe  work

--- GameTastes/main/views.py
+++ GameTastes/main/views.py
@@ -16,7 +16,7 @@ def recipe(request):
     return render(request, 'main/recipe.html', {'title': 'Рецепт'})
 
 
-@login_required(None, 'next', '/users/login')
+# @login_required(None, 'next', '/users/login')
 def account(request):
-    return render(request, 'main/account.html', {'title': 'Личный кабинет'})
+    return render(request, 'main/favourites.html', {'title': 'Личный кабинет'})
 
